d74b2fa2285464d6f935c5c7813671bf1faeca5a
Crowdbotics <team@crowdbotics.com>
2023-07-07 15:27:32 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "arranging-catalog-f-41978",
+  url: "https://arranging-catalog-f-41978.botics.co",
+  api: "https://arranging-catalog-f-41978.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
